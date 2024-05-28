de964f07ac851fe78e03f6da67d0b06e8356b486
Alex3925 <120993369+Alex3925@users.noreply.github.com>
2024-05-28 21:23:49 +0800

    Update master.js

--- commands/master.js
+++ commands/master.js
@@ -1,10 +1,10 @@
 module.exports = {
   name: 'mention',
-  description: 'Responds when "josh," "joshua," or "joshua apostol" is mentioned',
+  description: 'Responds when "alex," "alex ponce," or "alex jhon" is mentioned',
   nashPrefix: false,
   execute(api, event, args) {
     const message = event.body.toLowerCase();
-    if (message.includes('josh') || message.includes('joshua') || message.includes('joshua apostol')) {
+    if (message.includes('alex') || message.includes('alex ponce') || message.includes('alex jhon')) {
       api.sendMessage("Don't call my master, dude", event.threadID, event.messageID);
     }
   },
